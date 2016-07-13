1baf66743dc2eb6a476d5559a8916cde890dec61
Liang Yue Joseph Chin <CHIN@Liangs-MacBook-Pro.local>
2016-07-13 20:21:19 +0100

    adds powersaving mode and maximum temperatures

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -23,7 +23,35 @@ describe('Thermostat', function() {
   it('has a minimum of 10 degrees', function() {
     for(var i = 0; i < 11; i++) {
       thermostat.decreaseTemperature();
-    };
+    }
     expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+  it('has powersaving mode on by default', function() {
+    expect(thermostat.isPowerSavingOn()).toEqual(true);
+  });
+  it('can switch powersaving mode on again', function() {
+    thermostat.turnOffPSM();
+    expect(thermostat.isPowerSavingOn()).toEqual(false);
+    thermostat.turnOnPSM();
+    expect(thermostat.isPowerSavingOn()).toEqual(true);
+  });
+  it('has a maximum of 32 degrees', function() {
+    thermostat.turnOffPSM();
+    for(var i = 0; i < 13; i++) {
+      thermostat.increaseTemperature();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(32);
+  });
+  describe('when PSM is on', function() {
+    it('can switch powersaving mode off', function() {
+      thermostat.turnOffPSM();
+      expect(thermostat.isPowerSavingOn()).toEqual(false);
+    });
+    it('has a maximum of 25 degrees', function() {
+      for(var i = 0; i < 6; i++) {
+        thermostat.increaseTemperature();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
   });
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,12 +2,18 @@
 
 function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
+  this.MAXIMUM_TEMPERATURE = 32;
+  this.PSM_MAX_TEMPERATURE = 25;
   this._temperature = 20;
+  this._powerSaving = true;
 }
 Thermostat.prototype.getCurrentTemperature = function(song) {
   return this._temperature;
 };
 Thermostat.prototype.increaseTemperature = function () {
+  if(this.isMaximumTemperature()) {
+    return;
+  }
   this._temperature++;
 };
 Thermostat.prototype.decreaseTemperature = function () {
@@ -18,4 +24,20 @@ Thermostat.prototype.decreaseTemperature = function () {
 };
 Thermostat.prototype.isMinimumTemperature = function () {
   return this._temperature === this.MINIMUM_TEMPERATURE;
+};
+Thermostat.prototype.isMaximumTemperature = function () {
+  if(this._powerSaving === true) {
+    return this._temperature === this.PSM_MAX_TEMPERATURE;
+  } else {
+    return this._temperature === this.MAXIMUM_TEMPERATURE;
+  };
+};
+Thermostat.prototype.isPowerSavingOn = function () {
+  return this._powerSaving === true;
+};
+Thermostat.prototype.turnOffPSM = function () {
+  this._powerSaving = false;
+};
+Thermostat.prototype.turnOnPSM = function () {
+  this._powerSaving = true;
 };
